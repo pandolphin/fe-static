46396cd091d44b930c640eb8a10b3354f6c3c3ca
zicheng.lj <zicheng.lj@taobao.com>
2015-12-29 17:45:19 +0800

    updated

---
+++ src/es6/a.js
@@ -0,0 +1,4 @@
+var num1 = 1;
+var num2 = 2;
+
+export {num1, num2};
---
+++ src/es6/b.js
@@ -0,0 +1,7 @@
+import {num1, num2} from './a.js';
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(num1, num2));
